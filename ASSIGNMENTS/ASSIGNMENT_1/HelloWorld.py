a4f2171d30ae84035bd4518d96d39494f7a41e1a
Neel <neelraval78@gmail.com>
2023-01-27 13:57:18 -0600

    My Hello World Python file

---
+++ ASSIGNMENTS/ASSIGNMENT_1/HelloWorld.py
@@ -0,0 +1,12 @@
+# Full Name: Neel Raval 
+# Student ID: 1099835
+# Assignment Due Date: 01/29/2023
+# MSITM 6341:
+# Assignment #1
+# Assignment Title: Hello World
+# Helloworld.py
+
+print(" Hello World!")
+print("\n My name is Neel Raval")
+print("\n I am a python student")
+print("\n Have a nice day!")
