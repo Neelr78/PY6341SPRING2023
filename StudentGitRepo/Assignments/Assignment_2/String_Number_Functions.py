c989ef79bdfe5ddc3270fde6f21b6201c275b45d
Neel <neelraval78@gmail.com>
2023-02-03 15:39:39 -0600

    Assignment_2

---
+++ StudentGitRepo/Assignments/Assignment_2/String_Number_Functions.py
@@ -0,0 +1,20 @@
+# Full Name: Neel Raval
+# Student ID: 1099835
+# Assignment Due Date: 02/05/23, 11:59 PM (CST)
+# MSITM 6341:
+# Assignment #2
+# Assignment Title: String_Number_Functions
+# String_Number_Functions.py
+
+value1 = 40
+value2 = 45
+print("==============================")
+print("Result Dashboard")
+print("------------------------------")
+print("value 1 : " ,value1)
+print("\nvalue 2 : " ,value2)
+print("------------------------------")
+Total_summary = value1+value2
+print("Total Summary : ${:,.2f}".format(Total_summary))
+print("result : ${:,.2f}".format(Total_summary/7.6))
+print("==============================")
