d1781ef4c32a60e9da61214a39d15a159d7a146b
Neel <neelraval78@gmail.com>
2023-03-04 15:15:04 -0600

    Assignment 4

---
+++ StudentGitRepo/Assignments/Assignment_5/Working_with_if_statement.py
@@ -0,0 +1,40 @@
+# Full Name: Neel Raval
+# Student ID: 1099835
+# Assignment Due Date: 3/5/23, 11:59 PM (CST)
+# MSITM 6341:
+# Assignment #5
+# Assignment Title: working_with_if_statement
+# working_with_if_statement.py
+
+
+full_name = "Neel Raval"
+name2 = ""
+name3 = ""
+
+print()
+print("----------------------------")
+print()
+
+# Step 3
+print("My Full name is "+ full_name.title())
+print()
+
+# Step 5
+for letter in full_name:
+    # Step 5a
+    if letter.isupper():
+        letter = letter.lower()
+    else:
+        letter = letter.upper()
+    
+    name2 +=  letter + "-"
+    
+    name3 += str(ord(letter)) + "-" 
+    
+    print(f"ASCII value of {letter} is {ord(letter)}")
+
+print()
+print("-" +name2)
+print("-" +name3)
+
+print("----------------------------")
